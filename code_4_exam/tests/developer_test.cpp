bf21fd17e80a4c8c9a7fb24d63d6b54d41f4295f
nico <156666258+nicoDHBW@users.noreply.github.com>
2024-06-08 17:58:55 +0200

    some changes

---
+++ code_4_exam/tests/developer_test.cpp
@@ -0,0 +1,39 @@
+#include "junior_developer.hpp"
+#include "senior_developer.hpp"
+#include <gtest/gtest.h>
+
+class DeveloperTest : public ::testing::Test {
+protected:
+    JuniorDeveloper* junior;
+    SeniorDeveloper* senior;
+
+    void SetUp() override {
+        junior = new JuniorDeveloper("Peter Parker", "Spiderman");
+        senior = new SeniorDeveloper("Diana Prince", "Wonder Woman");
+    }
+
+    void TearDown() override {
+        delete junior;
+        delete senior;
+    }
+};
+
+TEST_F(DeveloperTest, TestLogoLoading) {
+    junior->load_logo_from_file("../assets/logo.txt");
+    EXPECT_FALSE(junior->get_logo().empty());
+
+    EXPECT_THROW(junior->load_logo_from_file("invalid_file.txt"), std::runtime_error);
+}
+
+TEST_F(DeveloperTest, TestSolveProblem) {
+    testing::internal::CaptureStdout();
+    junior->solve_problem();
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_NE(output.find("Puh, that was tough"), std::string::npos);
+    EXPECT_NE(output.find("Ahhhh, I needed that coffee!!!"), std::string::npos);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
