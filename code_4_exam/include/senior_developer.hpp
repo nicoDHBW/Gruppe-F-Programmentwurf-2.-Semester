9035ab4dc09585ec098ee1f9b049e47db0e1b5ab
nikolajschmidt <156666484+nikolajschmidt@users.noreply.github.com>
2024-06-15 16:16:37 +0200

    Commenting senior_developer.hpp

--- code_4_exam/include/senior_developer.hpp
+++ code_4_exam/include/senior_developer.hpp
@@ -3,10 +3,13 @@
 
 #include "developer.hpp"
 
+// Derived class for Senior Developer
 class SeniorDeveloper : public Developer {
 public:
+    // Constructor to initialize name and alias of the senior developer
     SeniorDeveloper(const std::string& name, const std::string& alias);
 
+    // Override the solve_problem method to provide a specific implementation for senior developers
     void solve_problem() override;
 };
 
