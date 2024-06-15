e1687fe1e729887e747acd33e82c0a82c30b9f64
nikolajschmidt <156666484+nikolajschmidt@users.noreply.github.com>
2024-06-15 16:18:10 +0200

    Commenting junior_developer.cpp

--- code_4_exam/src/junior_developer.cpp
+++ code_4_exam/src/junior_developer.cpp
@@ -1,8 +1,13 @@
 #include "junior_developer.hpp"
 
+// Constructor for the JuniorDeveloper class. Initializes the name and alias of the junior developer
+// by calling the base class (Developer) constructor.
 JuniorDeveloper::JuniorDeveloper(const std::string& name, const std::string& alias)
     : Developer(name, alias) {}
 
+// Implementation of the solve_problem method for JuniorDeveloper.
+// Outputs a message indicating that a problem is being solved and that it was tough.
+// Calls the drink_coffee method afterward.
 void JuniorDeveloper::solve_problem() {
     std::cout << "Solving a problem:\n" << *this << "\nPuh, that was tough\n";
     drink_coffee();
