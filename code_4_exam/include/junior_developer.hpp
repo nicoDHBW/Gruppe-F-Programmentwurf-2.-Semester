650c9cdc7743450deaf268087b7b0e0b936fb053
nikolajschmidt <156666484+nikolajschmidt@users.noreply.github.com>
2024-06-15 16:15:20 +0200

    Commenting junior_developer.hpp

--- code_4_exam/include/junior_developer.hpp
+++ code_4_exam/include/junior_developer.hpp
@@ -3,11 +3,14 @@
 
 #include "developer.hpp"
 
+// Derived class for Junior Developer
 class JuniorDeveloper : public Developer {
 public:
+    // Constructor to initialize name and alias of the junior developer
     JuniorDeveloper(const std::string& name, const std::string& alias);
 
+    // Override the solve_problem method to provide a specific implementation for junior developers
     void solve_problem() override;
-};
+}; 
 
 #endif // JUNIOR_DEVELOPER_HPP
