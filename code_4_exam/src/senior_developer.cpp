0753e2d2780fd491866376a1c760d6163ecb48c7
nikolajschmidt <156666484+nikolajschmidt@users.noreply.github.com>
2024-06-15 16:19:43 +0200

    Commenting senior_developer.cpp

--- code_4_exam/src/senior_developer.cpp
+++ code_4_exam/src/senior_developer.cpp
@@ -1,8 +1,13 @@
 #include "senior_developer.hpp"
 
+// Constructor for the SeniorDeveloper class. Initializes the name and alias of the senior developer
+// by calling the base class (Developer) constructor.
 SeniorDeveloper::SeniorDeveloper(const std::string& name, const std::string& alias)
     : Developer(name, alias) {}
 
+// Implementation of the solve_problem method for SeniorDeveloper.
+// Outputs a message indicating that a problem is being solved and that it was easy.
+// Calls the drink_coffee method afterward.
 void SeniorDeveloper::solve_problem() {
     std::cout << "Solving a problem:\n" << *this << "\nHa, that was EZ!\n";
     drink_coffee();
